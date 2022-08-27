1079fc87159f95462595415fbcb9901332d9459e
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-08-27 21:04:49 +0100

    a function that replaces an element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    n = len(my_list)
-    cpy = my_list.copy()
-    if idx < 0:
-        return _cpy
-    elif idx > n - 1:
-        return cpy
-    else:
-        cpy[idx] = element
-        return cpy
+    tmp_list = my_list[:]
+    if 0 <= idx < len(my_list):
+        tmp_list[idx] = element
+        return(tmp_list)
+    return(my_list)
