800be2701cf699c602d3d75cf95d34c8bbf09d1a
Wreford <wrefordmessi@gmail.com>
2022-08-28 04:13:17 -0700

    Linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * main - the system entry point.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+
+	return (0);
+}
