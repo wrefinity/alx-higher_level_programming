903cf0e25d1a3b13e59fab36804a99c2053e573e
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-08-31 19:31:57 +0100

    error fixes

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(my_dict, value):
-    tar = []
-    for k, v in my_dict.items():
-        if k is value:
-            targets.append(k)
-    for x in tar:
+    arr = []
+    for key, key_value in my_dict.items():
+        if key_value is value:
+            arr.append(key)
+    for x in arr:
         del my_dict[x]
     return(my_dict)
