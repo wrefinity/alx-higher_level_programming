3779345452562589808db39b7011008c7663a16e
Wreford <wrefordmessi@gmail.com>
2022-09-12 03:49:03 -0700

    inclusion of try except block

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,24 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     list_len = 0
-    n_element = 0
-
-    for _ in my_list:
-        list_len += 1
-    try:
-        if (x == 0):
-            return n_element
-        if (x >= list_len):
-            for e in my_list:
-                print("{}".format(e), end ="")
-            print()
-            return list_len
-        else:
-            for e in my_list:
-                n_element += 1
-                print("{}".format(e), end="")
-                if (n_element == x):
-                    break
-            print()
-    except BaseException:
-        pass
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            list_len += 1
+        except IndexError:
+            break
+    print()
+    return list_len
