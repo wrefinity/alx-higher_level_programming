8037477500a9d0e0dfa758e14182ff6fa9af988a
Wreford <wrefordmessi@gmail.com>
2022-09-19 14:52:49 -0700

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Defines a class Rectangle
+ Rectangle class module
 """
 
 
 class Rectangle:
-    """Representation of a rectangle"""
+    """
+    Rectangle class
+    """
     def __init__(self, width=0, height=0):
-        """initializing the rectangle by
-        setting the objet with width and height
-        """
+        """initializing the rectangle parameter"""
         self.width = width
         self.height = height
 
