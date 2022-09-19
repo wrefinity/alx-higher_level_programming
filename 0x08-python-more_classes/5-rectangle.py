76bf214ac3c3224d5aca58abe161cf220fc4215b
Wreford <wrefordmessi@gmail.com>
2022-09-19 14:57:47 -0700

    Detect instance deletion

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-Defines a class Rectangle
+ Rectangle class module
 """
 
 
 class Rectangle:
-    """Representation of a rectangle"""
+    """
+    Rectangle class
+    """
     def __init__(self, width=0, height=0):
-        """initializing the rectangle by
-        setting the objet with width and height
-        """
+        """initializing the rectangle parameter"""
         self.width = width
         self.height = height
 
     def __del__(self):
-        """prints a string when an instance has been deleted"""
+        """prints a string on an instance deleted"""
         print("Bye rectangle...")
 
     @property
