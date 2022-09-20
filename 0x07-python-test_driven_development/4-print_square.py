30bc0062ff2bf9836dbb9bc77a09500a018719dd
Wreford <wrefordmessi@gmail.com>
2022-09-20 15:57:53 -0700

    Write a function that prints a square with the character #.

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,7 +17,7 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        for j in range(size):
+    for _ in range(size):
+        for _ in range(size):
             print("#", end='')
         print()
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,28 +15,28 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    split_text_list = list()
-    new_text = list()
-    letters_list = list()
+    letters = []
+    text_split = []
+    new_list = []
 
-    for character in text:
-        letters_list.append(character)
-        if character in ['.', '?', ':']:
-            split_text_list.append(''.join(letters_list))
-            letters_list.clear()
+    for ch in text:
+        letters.append(ch)
+        if ch in ['.', '?', ':']:
+            text_split.append(''.join(letters))
+            letters.clear()
 
-    if letters_list:
-        split_text_list.append("".join(letters_list))
-    letters_list.clear()
+    if letters:
+        text_split.append("".join(letters))
+    letters.clear()
 
-    for sentence in split_text_list:
-        new_text.append(sentence.strip(' '))
-    split_text_list.clear()
+    for ch in text_split:
+        new_list.append(ch.strip(' '))
+    text_split.clear()
 
-    for character in new_text[-1]:
-        if character in ['.', '?', ':']:
-            print("\n\n".join(new_text))
+    for ch in new_list[-1]:
+        if ch in ['.', '?', ':']:
+            print("\n\n".join(new_list))
             print()
             return
 
-    print("\n\n".join(new_text), end='')
+    print("\n\n".join(new_list), end='')
