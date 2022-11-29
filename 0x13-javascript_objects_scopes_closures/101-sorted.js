b42248d7ed5f3fb4a9c70507e07e5b796277f1c7
wrefinity <wrefordmessi@gmail.com>
2022-11-29 22:50:55 +0000

    last bug

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
 const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
-    }
-  }
-  newDict[valsUniq[j]] = list;
+
+for (const [key, val] of Object.entries(dict)) {
+  newDict[val] ? newDict[val].push(key) : (newDict[val] = [key]);
 }
-console.log(newDict)
+console.log(newDict);
