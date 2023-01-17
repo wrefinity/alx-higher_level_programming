4cd776dc64852bfb84026fb9eb208d18ee05ebcb
wrefinity <wrefordmessi@gmail.com>
2023-01-17 21:13:39 +0100

    read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
 const fs = require("fs");
-const filename = process.argv[2];
-
-fs.readFile(filename, "utf-8", (er, body) => {
-  if (er) {
-    console.log(er);
-  } else {
-    console.log(body);
-  }
+fs.readFile(process.argv[2], "utf8", function (error, content) {
+  console.log(error || content);
 });
