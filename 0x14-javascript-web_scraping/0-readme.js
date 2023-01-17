f272b0911a3e9deff9d7beb255717977fd23a7a4
wrefinity <wrefordmessi@gmail.com>
2023-01-17 20:38:45 +0100

    read file content

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-/* read and print content of a file */
 
 const fs = require("fs");
 const filename = process.argv[2];
-fs.readFile(filename, "utf8", (err, data) => {
-  if (err) console.log(err);
-  else console.log(data.toString());
+
+fs.readFile(filename, "utf-8", (er, content) => {
+  if (er) {
+    console.log(er);
+  } else {
+    console.log(content);
+  }
 });
